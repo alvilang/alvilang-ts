e406514a4567161fc8094bed7b557fd052f738bf
Hedi Kelesh <hedkel.git@gmail.com>
2025-03-05 22:55:23 +0100

    refactor: pop() and peek() now throw errors if the stack is empty instead of returning undefined

--- src/lib/Stack.ts
+++ src/lib/Stack.ts
@@ -7,6 +7,10 @@ export default class Stack<T> {
     return this.storage.length;
   }
 
+  public isEmpty(): boolean {
+    return this.size() == 0;
+  }
+
   public push(item: T): void {
     if (this.size() === this.capacity) {
       throw Error('Stack full');
@@ -15,11 +19,20 @@ export default class Stack<T> {
     this.storage.push(item);
   }
 
-  public pop(): T | undefined {
-    return this.storage.pop();
+  public pop(): T {
+    const popped = this.storage.pop();
+    if (popped === undefined) {
+        throw new Error('Stack is empty');
+    }
+
+    return popped;
   }
 
-  public peek(): T | undefined {
+  public peek(): T {
     return this.storage[this.size() - 1];
+  }
+
+  public iterator(): ArrayIterator<T> {
+    return this.storage.values();
   }
 }
