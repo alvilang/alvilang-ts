5cbbfd62ad6a788ae948ee992d865f713dc41a38
Christian Mattsson <47303717+chrille0313@users.noreply.github.com>
2025-04-01 11:58:50 +0200

    refactor: fix typo

--- src/algorithms/linearSearch.ts
+++ src/algorithms/linearSearch.ts
@@ -1,5 +1,5 @@
 //Simply loops through the array and returns the index of the target if found, otherwise returns -1
-export function linearSeach(arr: number[], target: number): number {
+export function linearSearch(arr: number[], target: number): number {
   for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i] === target) {
       return i;
