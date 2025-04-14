eec0ad29ddf6dab90c4ff962bba41b4d6bb492fc
Hedi-Kelesh <hedkel.git@gmail.com>
2025-04-14 19:44:36 +0200

    test: adjustment of dijkstra to use the highlight function

--- src/test.ts
+++ src/test.ts
@@ -10,8 +10,6 @@ import UndirectedGraph from './lib/UndirectedGraph';
 //const log = new Logger();
 /*
 function minimum(arr: number[], arrlog: ArrayLogger): number {
-  if (arr.length == 0) throw new Error('list is empty');
-
   const minIndex = arrlog.recordState(0, StateVariableType.POINTER, 'minIndex');
   for (
     let i = arrlog.recordState(1, StateVariableType.POINTER, 'i');
@@ -33,8 +31,6 @@ function minimum(arr: number[], arrlog: ArrayLogger): number {
 }
 
 function minimum2(arr: number[]): number {
-  if (arr.length == 0) throw new Error('list is empty');
-
   let minIndex = 0;
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] < arr[minIndex]) {
@@ -81,12 +77,12 @@ log.write();
 function toString(array: LoggedArray<any>): string {
   let str = "["
   let i = 0;
-  while(i < array.length-1) {
+  while(i < array.size()-1) {
     str += array.get(i);
     str += ", ";
     i++;
   }
-  if (array.length) str += array.get(i);
+  if (array.size()) str += array.get(i);
   str += "]";
   return str;
 }
@@ -94,7 +90,7 @@ function toString(array: LoggedArray<any>): string {
 
 
 function insertionSort<T>(array: LoggedArray<T>) {
-  for (let i = array.createIndex(1); i.get() < array.length; i.set(i.get() + 1)) {
+  for (let i = array.createIndex(1); i.get() < array.size(); i.set(i.get() + 1)) {
     array.scope(() => {
     let j = array.createIndex(i.get());
     while (j.get() > 0 && array.get(j.get() - 1) > array.get(j.get())) {
@@ -107,7 +103,7 @@ function insertionSort<T>(array: LoggedArray<T>) {
 
 
 function quickSort<T>(array: LoggedArray<T>) {
-  quickSortHelper(array, 0, array.length-1);
+  quickSortHelper(array, 0, array.size()-1);
 }
 
 function quickSortHelper<T>(array: LoggedArray<T>, low: number, high: number) {
@@ -119,26 +115,26 @@ function quickSortHelper<T>(array: LoggedArray<T>, low: number, high: number) {
 
   array.swap(pivotIndex, high);
 
-  array.logger.compare(left, CompareOperator.LE, right);
+  array.getLogger().compare(left, CompareOperator.LE, right);
   while(left.get() <= right.get()) {
     
     //find left number
-    array.logger.compare(left, CompareOperator.LE, right);
-    array.logger.compare([array, left.get()], CompareOperator.LT, [array, high]);
+    array.getLogger().compare(left, CompareOperator.LE, right);
+    array.getLogger().compare([array, left.get()], CompareOperator.LT, [array, high]);
     while ((left.get() <= right.get()) && (array.get(left) < array.get(high))) {
       left.set(left.get()+1);
     }
 
     //find left number
-    array.logger.compare(right, CompareOperator.GE, left);
-    array.logger.compare([array, right.get()], CompareOperator.GT, [array, high]);
+    array.getLogger().compare(right, CompareOperator.GE, left);
+    array.getLogger().compare([array, right.get()], CompareOperator.GT, [array, high]);
     while ((right.get() >= left.get()) && (array.get(right) > array.get(high))) {
       right.set(right.get()-1);
     }
 
     //if a swap should be performed
     //array.mark(left, right, () => {
-    array.logger.compare(left, CompareOperator.LT, right);
+    array.getLogger().compare(left, CompareOperator.LT, right);
     if (left.get() < right.get()) {
       array.swap(left, right);
       left.set(left.get()+1);
@@ -218,14 +214,15 @@ function dijkstra<V>(start: V, graph: LoggedGraph<V,number>) {
 
   const table: Map<V, CostPath<V>> = new Map<V, CostPath<V>>();
   //temporary solution to immitate minimum priority queue
-  const minQueue: WeightedEdge<V,number>[] = [];
+  const minQueue:  LoggedArray<WeightedEdge<V,number>> = graph.getLogger().createArray([]);
+  const comparator = (a: WeightedEdge<V,number>, b: WeightedEdge<V,number>) => b.weight - a.weight;
   //setup
   minQueue.push({src: start, dst: start, weight: 0});
   graph.getNodes().forEach(node => table.set(node, {known: false, cost: Infinity, path: null as V}));
   table.get(start)!.cost = 0;
 
   //start: table is 'empty' and none of the nodes are visited
-  while (minQueue.length > 0) {
+  while (minQueue.size() > 0) {
     console.log('Queue:        %o', minQueue);
 
     const currEdge = minQueue.pop()!;
@@ -236,6 +233,9 @@ function dijkstra<V>(start: V, graph: LoggedGraph<V,number>) {
     console.log('Table:        %o', table);
     console.log();
 
+
+    //highlight the node
+    graph.getLogger().highlight([[graph, currEdge.dst]], () => {
 
     //if node has not been visited
     if (!currNodeStatus.known) {
@@ -255,13 +255,19 @@ function dijkstra<V>(start: V, graph: LoggedGraph<V,number>) {
       //assign path to current node
       for (const [adjEdge, pointer] of graph.getEdgesFromNode(currEdge.dst)) {
         const adjNodeStatus = table.get(adjEdge.dst)!;
+
+        graph.getLogger().highlight([[graph, pointer]], () => {
         if (!adjNodeStatus.known /*&& adjNodeStatus.cost > currEdge.weight + adjEdge.weight*/ ) {
+          //highlight the edge
+          //graph.logger.highlight({graph, pointer});
           adjEdge.weight += currEdge.weight;
           minQueue.push(adjEdge);
         }
+        });
       }
-      minQueue.sort((a,b) => b.weight - a.weight);
+      minQueue.apply(Array.prototype.sort, comparator);
     }
+    });
   }
 
   return table;
@@ -288,6 +294,7 @@ log3.write('log3.json');
 console.log("\n" + toString(array4));
 */
 
+/*
 const log5 = new Logger();
 const b = [7,6,5,4,3,2,1];
 
@@ -296,10 +303,10 @@ let array5 = log5.createArray(b);
 quickSort(array5);
 log5.write('log5.json');
 console.log("\n" + toString(array5));
+*/
 
 
-
-
+/*
 const log6 = new Logger();
 const queue = log6.createQueue(6);
 //const stack = log6.createStack(6);
@@ -323,7 +330,7 @@ stack.pop();
 stack.push(4);
 stack.push(5);
 log7.write('stackTest.json');
-
+*/
 
 /*
 const log8 = new Logger();
@@ -362,7 +369,7 @@ console.log(tree.toString())
 console.log();
 */
 
-logger10.write('bst.json');
+//logger10.write('bst.json');
 
 /*
    2
@@ -381,7 +388,7 @@ adjList.addBiEdge('A', 'C', 4);
 adjList.addBiEdge('A', 'D', 3);
 adjList.addBiEdge('B', 'D', 7);
 adjList.addBiEdge('C', 'D', 5);
-Logger.registerAndWrite(Array.from(adjList.graph), 'graph1.json');
+//Logger.registerAndWrite(Array.from(adjList.graph), 'graph1.json');
 
 
 function adjListToGraph<V,E>(graph: DirectedGraph<V,E>): WeightedGraph<V,E> {
@@ -405,18 +412,18 @@ function adjListToGraph<V,E>(graph: DirectedGraph<V,E>): WeightedGraph<V,E> {
 }
 
 const graph = new LoggedGraph(0, new Logger(), adjListToGraph(adjList)).toValue();
-Logger.registerAndWrite(graph, 'graph2.json');
+//Logger.registerAndWrite(graph, 'graph2.json');
 
 const map = new Map<string, number>();
 map.set("a",1);
 map.set("b",2);
 map.set("c",3);
 map.set("d",4);
-Logger.registerAndWrite(Array.from(map), 'map.json');
+//Logger.registerAndWrite(Array.from(map), 'map.json');
 
 const array = [];
 array.push(3);
-Logger.registerAndWrite(array, 'array.json');
+//Logger.registerAndWrite(array, 'array.json');
 
 
 /*
@@ -473,6 +480,19 @@ adjList2.addEdge('H', 'D', 2);
 adjList2.addEdge('H', 'E', 6);
 adjList2.addEdge('H', 'F', 9);
 
-const dijkstraGraph = new LoggedGraph(0, new Logger(), adjListToGraph(adjList2));
+const logger12 = new Logger();
+const dijkstraGraph: LoggedGraph<string, number> = logger12.createGraph<string,number>(adjListToGraph(adjList2));
 console.log(dijkstra('B', dijkstraGraph));
+logger12.write('dijkstra.json');
 
+const logger11 = new Logger();
+const x = logger11.createArray([1]);
+logger11.startScope();
+logger11.endScope();
+logger11.startScope();
+  logger11.createArray([2]);
+  x.set(0,3);
+logger11.endScope();
+logger11.createArray([4]);
+
+logger11.write('scoping.json');
